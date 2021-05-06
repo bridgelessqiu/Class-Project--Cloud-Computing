70c7955c89e1c58cdd4849710a8844fd12148da3
bridgelessqiu <zq5au@virginia.edu>
2021-05-05 20:49:59 -0400

    new function added

--- algorithm/algo.py
+++ algorithm/algo.py
@@ -2,37 +2,40 @@ import numpy as np
 from scipy import sparse
 import networkx as nx
 
-def cascade(A, a, v_time, v_state, p, k):
-"""
-Description
------------
-This function computes the cascade of the infection where the dynamic
-is specified as shown in the paper.
+# -------------------- #
+#       Cascase        #
+# -------------------- #
+def cascade(A, v_time, v_state, p, k):
+    """
+    Description
+    -----------
+    This function computes the cascade of the infection where the dynamic
+    is specified as shown in the paper.
 
-Parameters
-----------
-A: n x n scipy sparse matrix
-   The adjacency matrix of the graph
+    Parameters
+    ----------
+    A: n x n scipy sparse matrix
+        The adjacency matrix of the graph
 
-p: float (0, 1)
-   The default transmission probability
+    p: float (0, 1)
+        The default transmission probability
 
-v_time: n x 1 numpy vector
-   The initial infection time of each vertex
+    v_time: n x 1 numpy vector
+        The initial infection time of each vertex
 
-v_state: n x 1 numpy vector
-   The initial infection state of each vertex
+    v_state: n x 1 numpy vector
+        The initial infection state of each vertex
 
-k: integer > 0
-   The maximum number of iterations
+    k: integer > 0
+        The maximum number of iterations
 
-Output
-------
-v_time: n x 1 numpy array
-   The vector that consists of the infection time of each vertex
-v_state: n x 1 numpy array
-   The vector that consists of the infection state of each vertex
-"""
+    Output
+    ------
+    v_time: n x 1 numpy array
+        The vector that consists of the infection time of each vertex
+    v_state: n x 1 numpy array
+        The vector that consists of the infection state of each vertex
+    """
     
     # The number of vertices
     n = np.shape(A)[0]
@@ -89,8 +92,74 @@ v_state: n x 1 numpy array
             return v_state, v_time
 
 
+# ------------------------------------------------------ #
+#       Learn the structure of bidirectional tree        #
+# ------------------------------------------------------ #
+def learn_tree_structure(A, p, k, num_of_cascade):
+    """
+    Description
+    -----------
+    This algorithm recovers the structures of trees under the extreme noise setting. For deails, please see the paper
+    """
+
+    n = np.shape(A)[0]
+    H = {} # As suggested in the paper, this is the fraction of cascades for which both i and j were infected
+    
+    # Initilize all to 0
+    for i in range(n):
+        for j in range(i, n):
+            H[(i, j)] = 0
+    
+    # Run MANY cascades
+    for _ in range(num_of_cascade):
+        v_state = [] # Initially only one infected vertex
+        v_time = [] # Initially only one infected vertex
+        v_state, v_time = cascade(A, v_time, v_state, p, k) # run the cascade
+       
+        for i in range(n):
+            for j in range(i, n):
+                if v_state[i] == v_state[j] == 1:
+                    H[(i, j)] += 1
+    
+    # Sort H in descending order
+    sorted_H = dict(sorted(H.items(), key=lambda item: item[1],  reverse = True))
+    
+    # Avoid forming cycles
+    selected = [0] * n
+    edges = []
+    total = 0
+    for key, value in sorted_H.items():
+        if total != n:
+            u = key[0]
+            v = key[1]
+            if (selected[u] == 0) or (selected[v] == 0):
+                edges.append((u, v))
+                selected[u] = 1
+                selected[v] = 1
+            total += 1
+        eles:
+            break
+   
+    # Compute EC
+    num_of_correct_edges = 0
+    A_dense = sparse.csr_matrix.todense(A)
+    for e in edges:
+        u = e[0]
+        v = e[1]
+        if A[u, v] == 1:
+            num_of_correct_edges += 1
+
+    EC = float(num_of_correct_edges / (n-1))
+
+    return EC
 
 
+
+# ---------------------------------------------------- #
+#        Learn the weights of bidirectional tree        #
+# ---------------------------------------------------- #
+def 
+        
 
     
     
---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+import algorithm.algo as aa
+
+if __name__ == "__main__":
+    f = aa.cascade
+
+    f()
